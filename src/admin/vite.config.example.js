de2272f340f106aabb2a571759d327ff0a2bb209
Apostolado <apostoladonewman@gmail.com>
2025-08-21 22:01:56 -0300

    first commit

---
+++ src/admin/vite.config.example.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+const { mergeConfig } = require('vite');
+
+module.exports = (config) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  });
+};
